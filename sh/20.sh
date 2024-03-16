dae5ff73dd5f2fcad36549858997345ec4ffc59c
ImSreyasCollege <sreyassatheesh755@gmail.com>
2024-03-16 13:51:08 +0530

    program 20 changed.

--- sh/20.sh
+++ sh/20.sh
@@ -2,8 +2,11 @@
 
 echo "Enter a number:"
 read num
-fact=1
-for ((i=1; i<=num; i++)); do
-    fact=$((fact * i))
-done
-echo "Factorial of $num is: $fact"
+fact(){
+    fact=1
+    for((i=1; i<=num; i++)); do 
+        fact=$((fact * i))
+    done
+    echo $fact
+}
+echo "Factorial of $num is: $(fact)"
