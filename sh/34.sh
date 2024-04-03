5d59a842e4c57209f9a799f3ab3984b3feb0aba6
ImSreyasCollege <sreyassatheesh755@gmail.com>
2024-04-03 11:37:05 +0530

    Program 34 completed

--- sh/34.sh
+++ sh/34.sh
@@ -2,11 +2,16 @@
 
 echo "Enter strings separated by space:"
 read -a strings
-for ((i=0; i<${#strings[@]}; i++)); do
-    rev=$(echo ${strings[i]} | rev)
-    reversed_strings[$i]=$rev
+
+leng=${#strings[@]}
+echo $leng
+reversed_strings=()
+for ((i=($leng-1); i>=0; i--)); do
+    reversed_strings+=(${strings[i]})
 done
-echo "Reversed list of strings:"
+
+echo "Initial array : ${strings[@]}"
+echo -n "Reversed list of strings: "
 for string in "${reversed_strings[@]}"; do
-    echo "$(echo $string | rev)"
-done
+    echo -n "$(echo $string | rev) "
+done; echo
