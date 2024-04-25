64db32895ba64c9b88e04dc601386b4e4170d9e8
ImSreyas <sreyassatheesh755@gmail.com>
2024-04-25 19:11:03 +0530

    Script 32 improved.

--- sh/32.sh
+++ sh/32.sh
@@ -2,20 +2,29 @@
 
 echo "Enter password:"
 read password
+isStrong=true
 if [[ ${#password} -lt 8 ]]; then
     echo "Password length should be at least 8 characters"
-    exit 1
+    isStrong=false
+    # exit 1
 fi
 if ! [[ $password =~ [0-9] ]]; then
     echo "Password should contain at least one digit"
-    exit 1
+    isStrong=false
+    # exit 1
 fi
 if ! [[ $password =~ [A-Z] ]]; then
     echo "Password should contain at least one uppercase letter"
-    exit 1
+    isStrong=false
+    # exit 1
 fi
 if ! [[ $password =~ [a-z] ]]; then
     echo "Password should contain at least one lowercase letter"
-    exit 1
+    isStrong=false
+    # exit 1
 fi
-echo "Password is strong"
+if $isStrong; then 
+    echo "Password is strong..."
+else 
+    echo "Password is not strong..."
+fi
