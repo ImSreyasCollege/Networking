f586846d1d474c4bce7020d690f13355d83b9bdf
ImSreyas <sreyassatheesh755@gmail.com>
2024-04-25 20:28:55 +0530

    Code improvement p32

--- sh/32.sh
+++ sh/32.sh
@@ -6,22 +6,18 @@ isStrong=true
 if [[ ${#password} -lt 8 ]]; then
     echo "Password length should be at least 8 characters"
     isStrong=false
-    # exit 1
 fi
 if ! [[ $password =~ [0-9] ]]; then
     echo "Password should contain at least one digit"
     isStrong=false
-    # exit 1
 fi
 if ! [[ $password =~ [A-Z] ]]; then
     echo "Password should contain at least one uppercase letter"
     isStrong=false
-    # exit 1
 fi
 if ! [[ $password =~ [a-z] ]]; then
     echo "Password should contain at least one lowercase letter"
     isStrong=false
-    # exit 1
 fi
 if $isStrong; then 
     echo "Password is strong..."
