a8c2f6f35a29932ff4002a4fdea257de3f446daa
ImSreyas <sreyassatheesh755@gmail.com>
2024-07-01 22:57:38 +0530

    script 7 modified

--- sh/07.sh
+++ sh/07.sh
@@ -2,17 +2,18 @@
 echo "Enter the coefficients (a, b, c) of the quadratic equation (ax^2 + bx + c):"
 read a b c
 discriminant=$((b * b - 4 * a * c))
+# discriminant=$(echo "$b * $b - 4 * $a * $c" | bc -l)
 if [ $discriminant -gt 0 ]; then
-    root1=$(echo "(-$b + sqrt($discriminant)) / (2 * $a)" | bc -l)
-    root2=$(echo "(-$b - sqrt($discriminant)) / (2 * $a)" | bc -l)
+    root1=$(echo "(-($b) + sqrt($discriminant)) / (2 * $a)" | bc -l)
+    root2=$(echo "(-($b) - sqrt($discriminant)) / (2 * $a)" | bc -l)
     echo "The roots are real and different."
     echo "Root 1 = $root1, Root 2 = $root2"
 elif [ $discriminant -eq 0 ]; then
-    root=$(echo "-$b / (2 * $a)" | bc -l)
+    root=$(echo "-($b) / (2 * $a)" | bc -l)
     echo "The roots are real and equal."
     echo "Root 1 = Root 2 = $root"
 else
-    real_part=$(echo "-$b / (2 * $a)" | bc -l)
+    real_part=$(echo "-($b) / (2 * $a)" | bc -l)
     imaginary_part=$(echo "sqrt($((-1 * discriminant))) / (2 * $a)" | bc -l)
     echo "The roots are complex and different."
     echo "Root 1 = $real_part + $imaginary_part i"
